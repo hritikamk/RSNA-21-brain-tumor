d5ef1057e0f5474bec83b05754df8091c2d0d97b
someshfengde <someshfengde@gmail.com>
2021-11-08 23:06:33 +0530

    asdfas

--- run.py
+++ run.py
@@ -220,10 +220,10 @@ def run_app():
     directory = os.getcwd()
     print(directory)
     print(os.listdir(directory))
-    anima = create_animation(load_dicom_line("./00001/FLAIR"))
+    # anima = create_animation(load_dicom_line("./00001/FLAIR"))
     #HtmlFile = line_ani.to_html5_video()
-    with open("myvideo.html", "w") as f:
-        print(anima.to_html5_video(), file=f)
+    # with open("myvideo.html", "w") as f:
+    #     print(anima.to_html5_video(), file=f)
     col1, col2 = st.columns(2)
     col3, col4 = st.columns(2)
     HtmlFile = open("myvideo.html", "r")
