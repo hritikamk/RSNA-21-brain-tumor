c10fbea1d16a418ed177b64584e5869849e1ade4
someshfengde <someshfengde@gmail.com>
2022-04-01 13:20:41 +0530

    sad

--- run.py
+++ run.py
@@ -162,7 +162,7 @@ def load_model():
     model = Model()
     model.to(device)
     checkpoint = torch.load(f"./best-model-1.pth",
-                            map_location=torch.device('cpu'))
+                            map_location=torch.device(device))
     model.load_state_dict(checkpoint["model_state_dict"])
     return model
 
