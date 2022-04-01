cc9f89f721367de0c59bd156fbd40f9138fc4643
someshfengde <someshfengde@gmail.com>
2022-04-01 13:08:40 +0530

    asdfyui

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from torch.utils.data import Dataset, DataLoader
 from introd import *
 from about import *
 from run import *
-
+import os 
 
 st.title('RSNA-21 Brain tumor detection')
-
+st.header(os.listdir("./"))
 x = st.sidebar.selectbox("choose app mode", ['Introduction', 'RUN >', 'ABOUT'])
 
 if x == 'Introduction':
